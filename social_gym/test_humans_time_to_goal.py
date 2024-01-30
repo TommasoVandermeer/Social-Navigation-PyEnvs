07e5a6abf160b2c01a47be6df22e83b44bd7a59e
TommasoVandermeer <tommasovdm@gmail.com>
2024-01-30 14:34:10 +0100

    Fixed bug for human times test

--- social_gym/test_humans_time_to_goal.py
+++ social_gym/test_humans_time_to_goal.py
@@ -25,10 +25,10 @@ RESULTS_FILES = ["bp_on_orca.pkl","bp_on_sfm_guo.pkl","bp_on_hsfm_new_guo.pkl","
 OUTPUT_FILE_WITH_ROBOT = "human_times_with_robot.pkl"
 HUMAN_STATES_FILE = "human_states_tests_without_robot.pkl"
 OUTPUT_FILE_WITHOUT_ROBOT = "human_times_without_robot.pkl"
-SKIP_PHASE_1 = False # If true, data of experiments with robot will be extracted from the OUTPUT_FILE_WITH_ROBOT specified, otherwise data will be computed from passed RESULTS_FILES
+SKIP_PHASE_1 = True # If true, data of experiments with robot will be extracted from the OUTPUT_FILE_WITH_ROBOT specified, otherwise data will be computed from passed RESULTS_FILES
 SKIP_PHASE_2 = False # If true, data of experiments without robot will be extracted from the HUMAN_STATES_FILE specified, otherwise tests without humans will be executed
 SKIP_PHASE_3 = True # If true, humans' time to goal is not extracted from experiments without robot
-HEADLESS = True
+HEADLESS = False
 ### IMPLEMENTATION VARIABLES (do not change)
 ENVIRONMENTS = ["orca", "sfm_guo", "hsfm_new_guo"]
 TESTS = ["5_humans","7_humans","14_humans","21_humans","28_humans","35_humans"]
@@ -85,12 +85,19 @@ if not SKIP_PHASE_1:
     # Save extracted data in an output file
     with open(os.path.join(metrics_dir,OUTPUT_FILE_WITH_ROBOT), "wb") as f: pickle.dump(extracted_data, f); f.close()
 
-### PHASE 2: Simulate episodes without robot
-# Load data with robot
+### PHASE 2: Simulate episodes without robot - WARNING: ensure humans' initial positions are the same
+# Load extracted data with robot
 if SKIP_PHASE_1: 
     with open(os.path.join(metrics_dir,OUTPUT_FILE_WITH_ROBOT), "rb") as f: data_with_robot = pickle.load(f)
 else: data_with_robot = extracted_data.copy()
 if not SKIP_PHASE_2:
+    # Extract initial pose of humans from results file to replicate the experimental setup
+    with open(os.path.join(os.path.dirname(__file__),'tests','results',RESULTS_FILES[0]), "rb") as f: tests_with_robot_data = pickle.load(f)
+    humans_initial_poses = []
+    for test in TESTS: 
+        results = tests_with_robot_data[test]["results"]
+        test_humans_initial_poses = [results[l]["human_states"][0,:,0:3] for l in range(len(results))]
+        humans_initial_poses.append(test_humans_initial_poses)
     # Setup logging
     base_dir = os.path.join(os.path.dirname(__file__),"tests")
     if not os.path.exists(base_dir): os.makedirs(base_dir)
@@ -110,27 +117,86 @@ if not SKIP_PHASE_2:
             circle_radius = test_data[test]["specifics"]["circle_radius"]
             runge_kutta = test_data[test]["specifics"]["runge_kutta"]
             time_step = test_data[test]["specifics"]["time_step"]
+            robot_time_step = test_data[test]["specifics"]["robot_time_step"]
             max_time = test_data[test]["specifics"]["max_episode_time"]
             episode_steps = max_time / time_step
             test_human_states = []
             for trial in range(n_trials):
                 if trial % 20 == 0: logging.info(f"Start trial {trial+1}")
-                np.random.seed(seed_offset + trial)
-                simulator = SocialNavSim([circle_radius,n_humans,True,env,HEADLESS,runge_kutta,False,False,False], "circular_crossing")
+                humans = {}
+                for h in range(n_humans): humans[h] = {"pos": humans_initial_poses[k][trial][h][0:2] ,"yaw": humans_initial_poses[k][trial][h][2],"goals": [-humans_initial_poses[k][trial][h][0:2],humans_initial_poses[k][trial][h][0:2]]}
+                config = {"headless": HEADLESS, "motion_model": env, "runge_kutta": runge_kutta, "robot_visible": False, "grid": True, "humans": humans, "walls": []}
+                simulator = SocialNavSim(config, "custom_config")
                 simulator.set_time_step(time_step)
                 # Run episode of predefined duration
-                human_states = simulator.run_k_steps(int(episode_steps), save_states_time_step=time_step)
+                human_states = simulator.run_k_steps(int(episode_steps), save_states_time_step=robot_time_step)
                 # Save data
                 test_human_states.append(human_states)
             env_data_without_robot[test] = test_human_states
         extracted_data_without_robot[env] = env_data_without_robot
     # Save extracted data in an output file
-    with open(os.path.join(metrics_dir,HUMAN_STATES_FILE), "wb") as f: pickle.dump(extracted_data_without_robot, f); f.close()
+    with open(os.path.join(os.path.dirname(__file__),'tests','results',HUMAN_STATES_FILE), "wb") as f: pickle.dump(extracted_data_without_robot, f); f.close()
 
 ### PHASE 3: From experiments of humans without robot extract humans' time to goal
 # Load data without robot
 if SKIP_PHASE_2:
-    with open(os.path.join(metrics_dir,HUMAN_STATES_FILE), "rb") as f: data_without_robot = pickle.load(f)
+    with open(os.path.join(os.path.dirname(__file__),'tests','results',HUMAN_STATES_FILE), "rb") as f: data_without_robot = pickle.load(f)
 else: data_without_robot = extracted_data_without_robot.copy()
-if not SKIP_PHASE_3: pass
+if not SKIP_PHASE_3: 
+    extracted_data = {}
+    for i, results in enumerate(RESULTS_FILES):
+        results_file_extracted_data = {}
+        test_data_with_robot = data_with_robot.copy()[results]
+        for k, test in enumerate(test_data_with_robot):
+            print(" ************************ ")
+            print("")
+            test_extracted_data = {}
+            n_humans = int(test.replace("_humans",""))
+            # Extract data from test_data_with_robot
+            episode_times_with_robot = np.copy(test_data_with_robot[test]["episode_times"])
+            environment = test_data_with_robot[test]["specifics"]["human_policy"]
+            time_step = test_data_with_robot[test]["specifics"]["robot_time_step"]
+            # Initialize output variables
+            human_time_to_goal = np.empty((test_data_with_robot[test]["specifics"]["trials"],n_humans), dtype=np.float64); human_time_to_goal[:] = np.NaN
+            n_humans_reached_goal = np.zeros((test_data_with_robot[test]["specifics"]["trials"],), dtype=int)
+            episode_times = np.zeros((test_data_with_robot[test]["specifics"]["trials"],), dtype=np.float64)
+            for e in range(test_data_with_robot[test]["specifics"]["trials"]):
+                human_goals = np.empty((n_humans,2), dtype=np.float64)
+                human_reached_first_goal = [False for z in range(n_humans)]
+                # Take episode data of tests without robot
+                episode_human_states_no_robot = data_without_robot[environment][test][e]
+                steps = int((episode_times_with_robot[e] + time_step) / time_step)
+                for t in range(steps): # Loop until end of episode time (not all states available)
+                    human_states = episode_human_states_no_robot[t]
+                    for h, human_state in enumerate(human_states):
+                        # Take first human state position to compute humans' goal
+                        if t == 0: human_goals[h] = -human_state[0:2]
+                        else:
+                            if human_reached_first_goal[h]: continue
+                            human_position = human_state[0:2]
+                            human_radius = test_data_with_robot[test]["specifics"]["humans_radiuses"][h] # Assuming human radiuses are the same used for tests with robot
+                            distance_to_goal = np.linalg.norm(human_position - human_goals[h])
+                            # If human has reached the goal (due to the fact that we immediately change goal when humans reach it)
+                            if (distance_to_goal <= human_radius + (time_step / 2)) and not human_reached_first_goal[h]: # We assume humans desired speed is 1 m/s (should be human_radius + (robot_timestep * human_des_speed / 2))
+                                human_reached_first_goal[h] = True
+                                human_time_to_goal[e,h] = t * time_step
+                    if t == steps - 1:
+                        episode_times[e] = t * time_step
+                        n_humans_reached_goal[e] = sum(human_reached_first_goal)
+                        if episode_times[e] != episode_times_with_robot[e]: print("ERROR: episode duation is different from the test with the robot")
+            # Save final data
+            test_extracted_data["times_to_goal"] = human_time_to_goal
+            test_extracted_data["n_humans_reached_goal"] = n_humans_reached_goal
+            test_extracted_data["episode_times"] = episode_times
+            results_file_extracted_data[test] = test_extracted_data
+            # Print info
+            print(f"TEST {results} - {test}")
+            print(f"N° humans that reached the goal in {test_data_with_robot[test]['specifics']['trials']} trials: ", np.sum(n_humans_reached_goal, axis = 0))
+            print("Average humans time to goal: ", np.nansum(human_time_to_goal, axis=(0,1)) / np.sum(n_humans_reached_goal, axis = 0))
+            print(f"Average episode duration: {np.sum(episode_times, axis = 0) / test_data_with_robot[test]['specifics']['trials']}")
+            print("")
+        extracted_data[results] = results_file_extracted_data
+    # Save extracted data in an output file
+    with open(os.path.join(metrics_dir,OUTPUT_FILE_WITHOUT_ROBOT), "wb") as f: pickle.dump(extracted_data, f); f.close()
+
 
